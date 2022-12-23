d9552b1de032eadfb950e011424cd72260651da1
Egor <juventini109@gmail.com>
2022-12-23 17:20:08 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+let number = 5;
+const leftBorderWidth = 1;
+number = 11;
+console.log(number);
+
+const obj = {
+  name: 'Alex',
+  family: 'Smith'
+};
+obj.name = 'Egor';
+console.log(obj);
+
+// alert('Hello, Beach');
+const result = confirm('Are you here?');
+console.log(result);

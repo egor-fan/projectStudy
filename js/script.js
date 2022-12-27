f41932cb80c9981c017f42ab50f01fad958d1335
Egor <juventini109@gmail.com>
2022-12-27 16:59:22 +0300

    practic_2

--- js/script.js
+++ js/script.js
@@ -1,17 +1,42 @@
 "use strict";
 
-let number = 5;
-const leftBorderWidth = 1;
-number = 11;
-console.log(number);
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const obj = {
-  name: 'Alex',
-  family: 'Smith'
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 };
-obj.name = 'Egor';
-console.log(obj);
 
-// alert('Hello, Beach');
-const result = confirm('Are you here?');
-console.log(result);
+if (personalMovieDB.count < '10' && personalMovieDB.count != null && personalMovieDB.count != 0) {
+  alert('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= '10' && personalMovieDB.count <= '30') {
+  alert('Вы классический зритель');
+} else if (personalMovieDB.count > 30) {
+  alert('Вы киноман!');  
+} else {
+  alert('Произошла ошибка!');
+}
+
+for (let i = 0; i < 2; i++) {
+  const a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt('На сколько оцените его?', '');
+
+    if(a != '' && b != '' && a != null && b != null && a.length < 50 && b.length < 50) {
+      personalMovieDB.movies[a] = b;
+      console.log('done');
+    } else {
+      console.log('ERROR!');
+      i--;
+    }
+}
+
+
+console.log(personalMovieDB);
+
+
+
+
+
